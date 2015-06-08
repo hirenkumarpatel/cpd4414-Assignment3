faa0da85632ac1b3454a0256836b17513b5725f9
Hiren Patel <Hiren Patel@Hiren-PC.gateway.2wire.net>
2015-06-08 18:40:20 -0400

    implemented new test     testProcessWhenTimeReceivedIsSetThenTimeProcessedToNow() throws PurchaseException, CustomerException

--- src/java/classes/Product.java
+++ src/java/classes/Product.java
@@ -75,7 +75,7 @@ public class Product extends HttpServlet {
             } 
             else {
                 int id = Integer.parseInt(request.getParameter("ProductID"));
-                printer.println(resultMethod("SELECT * FROM product WHERE ProductID= ?", String.valueOf(id)));
+                printer.println(resultMethod("SELECT * FROM products WHERE Product_id= ?", String.valueOf(id)));
             }
 
         } catch (IOException ex) {
@@ -102,21 +102,30 @@ public class Product extends HttpServlet {
         try 
         {
             PrintWriter printer = response.getWriter();
-            if (keyValues.contains("ProductID") && keyValues.contains("name") && keyValues.contains("description")
-                    && keyValues.contains("quantity")) {
-                String ProductID = request.getParameter("ProductID");
-                String name = request.getParameter("name");
-                String description = request.getParameter("description");
-                String quantity = request.getParameter("quantity");
-                doUpdate("INSERT INTO product (ProductID,name,description,quantity) VALUES (?, ?, ?, ?)", ProductID, name, description, quantity);
+            
+            //checking for valid table fields
+            if (keyValues.contains("productId") && keyValues.contains("productName") && keyValues.contains("productDescription")          && keyValues.contains("productQuantity")) {
+                
+                
+                String productId = request.getParameter("productId");
+                String productName = request.getParameter("productName");
+                String productDescription = request.getParameter("productDescription");
+                String productQuantity = request.getParameter("productQuantity");
+                
+                doUpdate("insert into products "
+                        + "(product_id,product_name,product_description,product_quantity)"
+                        + " values (?, ?, ?, ?)", productId, productName, productDescription, productQuantity);
 
-            } else {
+            }
+            else 
+            {
+                //set status to internal srerver error..
                 response.setStatus(500);
-                printer.println("Error: Not data found for this input. Please use a URL of the form /servlet?name=XYZ&age=XYZ");
+                printer.println("No match data found for this input");
             }
 
         } catch (IOException ex) {
-            System.err.println("Input Output Issue in doPost Method: " + ex.getMessage());
+            System.err.println("IO Exception " + ex.getMessage());
         }
 
     }
@@ -131,5 +140,131 @@ public class Product extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+    
+    
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) {
+
+        Set<String> keySet = request.getParameterMap().keySet();
+        try (PrintWriter out = response.getWriter()) {
+            if (keySet.contains("ProductID") && keySet.contains("name") && keySet.contains("description") && keySet.contains("quantity")) {
+                String ProductID = request.getParameter("ProductID");
+                String name = request.getParameter("name");
+                String description = request.getParameter("description");
+                String quantity = request.getParameter("quantity");
+                doUpdate("update product set ProductID = ?, name = ?, description = ?, quantity = ? where ProductID = ?", ProductID, name, description, quantity, ProductID);
+            } else {
+                out.println("Error: Not data found for this input. Please use a URL of the form /products?id=xx&name=XXX&description=XXX&quantity=xx");
+            }
+        } catch (IOException ex) {
+            response.setStatus(500);
+            System.out.println("Error in writing output: " + ex.getMessage());
+        }
+    }
+
+    /**
+     * doDelete Method took two Arguments and This method Will delete Entries
+     * from product Table This will also catch SQLException and Display an error
+     * message
+     *
+     * @param request
+     * @param response
+     * @throws IOException
+     */
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Set<String> keySet = request.getParameterMap().keySet();
+        try (PrintWriter out = response.getWriter()) {
+            Connection conn = getConnection();
+            if (keySet.contains("ProductID")) {
+                PreparedStatement pstmt = conn.prepareStatement("DELETE FROM `product` WHERE `ProductID`=" + request.getParameter("ProductID"));
+                try {
+                    pstmt.executeUpdate();
+                } catch (SQLException ex) {
+                    System.err.println("SQL Exception Error in Update prepared Statement: " + ex.getMessage());
+                    out.println("Error in deleting entry.");
+                   
+                }
+            } else {
+                out.println("Error: Not enough data in table to delete");
+                
+            }
+        } catch (SQLException ex) {
+            System.err.println("SQL Exception Error: " + ex.getMessage());
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    /**
+     * resultMethod accepts two arguments It executes the Query get ProductID,
+     * name, description, quantity
+     *
+     * @param query
+     * @param params
+     * @throws SQLException
+     * @return
+     */
+    private String resultMethod(String query, String... params) {
+        StringBuilder sb = new StringBuilder();
+        String jsonString = "";
+        try (Connection conn = Credentials.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            ResultSet rs = pstmt.executeQuery();
+            List l1 = new LinkedList();
+            while (rs.next()) {
+                //Refernce Example 5-2 - Combination of JSON primitives, Map and List
+                //https://code.google.com/p/json-simple/wiki/EncodingExamples
+                Map m1 = new LinkedHashMap();
+                m1.put("ProductID", rs.getInt("ProductID"));
+                m1.put("name", rs.getString("name"));
+                m1.put("description", rs.getString("description"));
+                m1.put("quantity", rs.getInt("quantity"));
+                l1.add(m1);
+
+            }
+
+            jsonString = JSONValue.toJSONString(l1);
+        } catch (SQLException ex) {
+            System.err.println("SQL Exception Error: " + ex.getMessage());
+        }
+        return jsonString.replace("},", "},\n");
+    }
+
+    /**
+     * doUpdate Method accepts two arguments Update the entries in the table
+     * 'product'
+     *
+     * @param query
+     * @param params
+     * @return numChanges
+     */
+    private int doUpdate(String query, String... params) {
+        int numChanges = 0;
+        try (Connection conn = Credentials.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            numChanges = pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            System.err.println("SQL EXception in doUpdate Method" + ex.getMessage());
+        }
+        return numChanges;
+    }
+
+}
+
 
 }
